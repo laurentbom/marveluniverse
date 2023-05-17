54d31e68b0e05ffba1253b374e7639623a823ca6
LaurentB <laurent.bomal@gmail.com>
2023-05-17 12:51:17 +0200

    progress

---
+++ js/main.js
@@ -0,0 +1,48 @@
+const {createApp} = Vue;
+
+createApp({
+  data(){
+    return{
+      allDatas : [],
+      search : "",
+      searchResults: "",
+      isDetails : "",
+      heroDetails: [],
+    }
+  },
+  methods : {
+    getDatas(){
+      fetch('https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json')
+      .then(response => response.json())
+      .then(data => {
+        this.allDatas = data;
+      })
+      .catch(error => {
+        console.error('Une erreur s\'est produite:', error)
+      })
+    },
+    searchForHero(){
+      this.searchResults = this.allDatas.filter(superhero => superhero.name.toLowerCase().includes(this.search.toLowerCase()))
+      // console.log(this.searchResults);
+    },
+    getHeroDetails(hero){
+      this.isDetails = "moved";
+      this.heroDetails = [];
+      fetch(`https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/${hero}.json`)
+      .then(response => response.json())
+      .then(data => {
+        this.heroDetails = data;
+        console.log(this.heroDetails)
+      })
+      .catch(error => {
+        console.error('Une erreur s\'est produite:', error)
+      })
+    },
+    closeDetails(){
+      this.isDetails = "";
+    }
+  },
+  mounted() {
+    this.getDatas();
+  },
+}).mount("#app")
